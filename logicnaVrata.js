689b31b3ace7f7978763665be1488d4d7007fd15
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-01-05 06:19:35 +0000

    Doda ostale stevilske sisteme

--- logicnaVrata.js
+++ logicnaVrata.js
@@ -1,36 +1,41 @@
+let numSistem = ["0b", "0o", "", "0x"];
+let numSistem2 = [2, 8, 10, 16];
+let trenutniSistem = 3; //samo pri testiranju
+
+
 function racBit(prvo, operator, drugo = 0){
     if(operator == "&"){
-        let result = "0b" + (prvo & drugo).toString(2);
+        let result = numSistem[trenutniSistem] + (prvo & drugo).toString(numSistem2[trenutniSistem]); //Zamenjaj z lastno funkcijo za pretvorbo
         return result;
     }
     else if(operator == "|"){
-        let result = "0b" + (prvo | drugo).toString(2);
+        let result = numSistem[trenutniSistem] + (prvo | drugo).toString(numSistem2[trenutniSistem]);
         return result;
     }
     else if(operator == "^"){
-        let result = "0b" + (prvo ^ drugo).toString(2);
+        let result = numSistem[trenutniSistem] + (prvo ^ drugo).toString(numSistem2[trenutniSistem]);
         return result;
     }
     else if(operator == "~"){
-        len = prvo.length-2
-        result = "0b" + (~prvo >>> 0).toString(2);
-        result = "0b" + result.substring((result.length) - len);
+        len = prvo.length;
+        if(trenutniSistem != 2)
+            len -= 2;
+        result = numSistem[trenutniSistem] + (~prvo >>> 0).toString(numSistem2[trenutniSistem]);
+        result = numSistem[trenutniSistem] + result.substring((result.length) - len);
         return result;
     }
 }
-
-//console.log(racBit(27, "&", 13));
 
 function vecRacBit(izraz){
     izraz = izraz.split(" ");
 
     for (i in izraz){
-        if(izraz[i][0] == "0" || izraz[i][0] == "1"){
-            izraz[i] = "0b" + izraz[i];
+        if(izraz[i][0] != "~" && izraz[i][0] != "&" && izraz[i][0] != "|" && izraz[i][0] != "^"){
+            izraz[i] = numSistem[trenutniSistem] + izraz[i];
         }
         if(izraz[i][0] == "~"){
-            num = "0b" + izraz[i].substring(1);
-            izraz[i] = racBit(num, "~").toString(2);
+            num = numSistem[trenutniSistem] + izraz[i].substring(1);
+            izraz[i] = racBit(num, "~").toString(numSistem2[trenutniSistem]);
         }
     }
 
@@ -52,10 +57,13 @@ function vecRacBit(izraz){
     for(let i = 0; i < izraz.length; i++){
         result += izraz[i];
     }
+    if(trenutniSistem != 2)
+        result = result.substring(2);
     return result;
 }
 
-console.log(vecRacBit("~11011 | 10111 & 101"));
+console.log(vecRacBit("15 | 12 & 13"));
+console.log(vecRacBit("~a3b"));
+
 //Test
 //console.log(eval("~11011 | 10111 & 101"));
-//0b, 0o, 0x
