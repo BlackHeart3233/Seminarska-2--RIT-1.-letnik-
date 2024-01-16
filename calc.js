e76da6f3a1318fd44c26ea5fb3d3c7768b94bb17
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-01-16 09:52:21 +0000

    Add conversion calc GUI

--- calc.js
+++ calc.js
@@ -120,43 +120,6 @@ function racBit(prvo, operator, drugo = 0){
     }
 }
 
-//Obravnava celotnih izrazov
-function vecRacBit(izraz){
-    izraz = izraz.split(" ");
-
-    for (i in izraz){
-        if(izraz[i][0] != "~" && izraz[i][0] != "&" && izraz[i][0] != "|" && izraz[i][0] != "^"){
-            izraz[i] = numSistem[trenutniSistem] + izraz[i];
-        }
-        if(izraz[i][0] == "~"){
-            num = numSistem[trenutniSistem] + izraz[i].substring(1);
-            izraz[i] = racBit(num, "~").toString(numSistem2[trenutniSistem]);
-        }
-    }
-
-    let operator = ["&", "|", "^"];
-    for(let j = 0; j < 3; j++){
-        for(let i = 0; i < izraz.length; i++){
-            if (izraz[i] == operator[j]){
-                let result = racBit(Number(izraz[i-1]), operator[j], Number(izraz[i+1]));
-                izraz.splice(i-1, 3, result);
-                i = 0;
-            }
-            if (izraz.length <= 1){
-                break;
-            }
-        }
-    }
-
-    let result = "";
-    for(let i = 0; i < izraz.length; i++){
-        result += izraz[i];
-    }
-    if(trenutniSistem != 2)
-        result = result.substring(2);
-    return result;
-}
-
 //Testiranje
 //console.log(vecRacBit("15 | 12 & 13"));
 //console.log(vecRacBit("~a3b"));
