ba7cd7d13b83691f52b75ae02461acb19480f790
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-01-16 11:02:50 +0000

    Implement OCT and HEX button

--- calc.js
+++ calc.js
@@ -25,7 +25,7 @@ function IzbrisiEno(){
 
 let numSistem = ["0b", "0o", "", "0x"];
 let numSistem2 = [2, 8, 10, 16];
-let trenutniSistem = 1; 
+let trenutniSistem = 2; 
 let vrednosti = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
 let trijeBiti = ["000", "001", "010", "011", "100", "101", "110", "111"];
 let stirjeBiti = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"];
@@ -91,6 +91,25 @@ function decConversion(stevilo, sistem){ // sistem je število 2, 8 ali 16
     return result;
 }
 
+//OCT button
+function toOctConversion(){
+    if(numSistem2[trenutniSistem] == 8){
+        return;
+    }
+
+    let stevilo = document.getElementById("vnos").value;
+    if(numSistem2[trenutniSistem] == 2){
+        document.getElementById("vnos").value = binToOct(stevilo);
+    }
+    else if(numSistem2[trenutniSistem] == 10){
+        document.getElementById("vnos").value = decConversion(stevilo, 8);
+    }
+    else if(numSistem2[trenutniSistem] == 16){
+        return; //Ni bilo treba tega naret
+    }
+    trenutniSistem = 1;
+}
+
 function binToOct(stevilo){
     if(stevilo.length % 3 == 1){
         stevilo = "00" + stevilo;
@@ -115,7 +134,24 @@ function binToOct(stevilo){
     return result;
 }
 
+//HEX button
+function toHexConversion(){
+    if(numSistem2[trenutniSistem] == 16){
+        return;
+    }
 
+    let stevilo = document.getElementById("vnos").value;
+    if(numSistem2[trenutniSistem] == 2){
+        document.getElementById("vnos").value = binToHex(stevilo);
+    }
+    else if(numSistem2[trenutniSistem] == 8){
+        return; //Ni bilo treba tega naret
+    }
+    else if(numSistem2[trenutniSistem] == 10){
+        document.getElementById("vnos").value = decConversion(stevilo, 16);
+    }
+    trenutniSistem = 3;
+}
 
 function binToHex(stevilo){
     if(stevilo.length % 4 == 1){
@@ -147,8 +183,6 @@ function binToHex(stevilo){
 
 
 //LOGICNI OPERATORJI
-
-
 
 //Računanje posameznih operatorjev
 function racBit(prvo, operator, drugo = 0){
