d4378acb496f80d2d7298e824b430b6db9a554e3
BlackHeart3233 <80067641+BlackHeart3233@users.noreply.github.com>
2024-01-17 15:41:08 +0100

    Converter
    Not working:
     - DEC to OCT
    - BIN to OCT
    -OCT to HEX

--- pretvorbe.js
+++ pretvorbe.js
@@ -4,50 +4,38 @@ function Rezultat() {
     var VnosStevila = document.getElementById("vnos_stevila1").value
     if(IzSistema == "BIN"){
         BIN(VSistem, VnosStevila);
+    }else if(IzSistema == "DEC"){
+        DEC(VSistem, VnosStevila);
+    }else if(IzSistema == "OCT"){
+        OCT(VSistem, VnosStevila);
+    }else if(IzSistema == "HEX"){
+        HEX(VSistem, VnosStevila);
     }
+}
+
+function IzDECvBIN(VnosStevila){
+    var Stevilo_v_sistem ="";
+    let zacasno_st = 0;
+    let ostanek;
+    let novo_stevilo = +(VnosStevila);
+    list=["0","1"];
+    while (novo_stevilo != 0){
+        ostanek = novo_stevilo%2;
+        if(ostanek == list[0]){
+            Stevilo_v_sistem = list[0]+Stevilo_v_sistem;
+        }else{
+            Stevilo_v_sistem = list[1]+Stevilo_v_sistem;
+        }
+        zacasno_st = novo_stevilo/2;
+        novo_stevilo = zacasno_st | 0;
+
+    }
+    return Stevilo_v_sistem
 
 }
 
-function BIN(VSistem,VnosStevila) {
-    if(VSistem == "DEC"){
-        let stevec = 1;
-        var Stevilo_v_sistem = 0;
-        let dolzina_stevila = VnosStevila.length;
-        alert("im here")
 
-        for (let i = dolzina_stevila - 1; i >= 0; i--) {
-            let branje_stevila = VnosStevila[i];
-            if (branje_stevila === "1") {
-                Stevilo_v_sistem += stevec;
-            }
-            stevec *= 2;
-        }
-    }else if(VSistem == "OCT"){
-        let stevec = 1;
-        var Stevilo_v_sistem = "";
-        let stejem_stevilo = 0;
-        let dolzina_stevila = VnosStevila.length;
-        let stevec_spremembe = 0;
-        let list = ["0", "1", "2", "3", "4", "5", "6", "7"];
-        
-        for (let i = dolzina_stevila - 1; i >= 0; i--) {
-            let branje_stevila = VnosStevila[i];
-        
-            if (branje_stevila === "1") {
-                stejem_stevilo += stevec;
-            }
-            stevec *= 2;
-            stevec_spremembe += 1;
-            if (stevec_spremembe === 3) {
-                stevec_spremembe = 0;
-                stevec = 1;
-        
-                for (let l = 0; l < 8; l++) {
-                    if (list[l] == stejem_stevilo) {
-                        Stevilo_v_sistem = list[l] + Stevilo_v_sistem;}}
-                stejem_stevilo = 0;
-            }}   
-        }else if(VSistem == "HEX"){
+function IzBINvHEX(VnosStevila){
             let stevec = 1;
             var Stevilo_v_sistem = "";
             let stejem_stevilo = 0;
@@ -72,8 +60,133 @@ function BIN(VSistem,VnosStevila) {
                     stejem_stevilo = 0;
                 }
             }
-        }   
-    alert(Stevilo_v_sistem);     
+            return Stevilo_v_sistem;
+}
+
+function IzBINvOCT(VnosStevila){
+    let stevec = 1;
+    var Stevilo_v_sistem = "";
+    let stejem_stevilo = 0;
+    let dolzina_stevila = VnosStevila.length;
+    let stevec_spremembe = 0;
+    let list = ["0", "1", "2", "3", "4", "5", "6", "7"];
+    
+    for (let i = dolzina_stevila - 1; i >= 0; i--) {
+        let branje_stevila = VnosStevila[i];
+    
+        if (branje_stevila === "1") {
+            stejem_stevilo += stevec;
+        }
+        stevec *= 2;
+        stevec_spremembe += 1;
+        if (stevec_spremembe === 3) {
+            stevec_spremembe = 0;
+            stevec = 1;
+    
+            for (let l = 0; l < 8; l++) {
+                if (list[l] == stejem_stevilo) {
+                    Stevilo_v_sistem = list[l] + Stevilo_v_sistem;}}
+            stejem_stevilo = 0;
+        }}
+    return Stevilo_v_sistem;   
+
+}
+
+function IzHEXvBIN(VnosStevila){
+    let Stevilo_v_sistem ="";
+    let dolzina_stevila = VnosStevila.length;
+    let list = [["0","0000"], ["1","0001"], ["2","0010"], ["3","0011"], ["4","0100"], ["5","0101"], ["6","0110"] ,["7","0111"], ["8","1000"], ["9","1001"], ["A","1010"], ["B","1011"], ["C","1100"], ["D","1101"], ["E","1110"], ["F","1111"]];
+            
+    for (let i = dolzina_stevila - 1; i >= 0; i--) {
+        let branje_stevila = VnosStevila[i];
+        for(let l = 0; l<15;l++){
+            if(branje_stevila === list[l][0]){
+                Stevilo_v_sistem = list[l][1]+Stevilo_v_sistem;
+            }
+        }
+}
+return Stevilo_v_sistem
+}
+
+function IzOCTvBIN(VnosStevila){
+    var list = [[0, "000"], [1, "001"], [2, "010"], [3, "011"], [4, "100"], [5, "101"], [6, "110"], [7, "111"]];
+    var dolzina_stevila = VnosStevila.length;
+    var Stevilo_v_sistem = "";
+
+    for (let i = dolzina_stevila - 1; i >= 0; i--) {
+        let branje_stevila = parseInt(VnosStevila[i], 10); // Convert to a number
+        for (let l = 0; l < 8; l++) {
+            if (branje_stevila === list[l][0]) {
+                Stevilo_v_sistem = list[l][1] + Stevilo_v_sistem;
+            }
+        }
+    }
+    Stevilo_v_sistem = Stevilo_v_sistem.replace(/^0+/, '');
+    return Stevilo_v_sistem;
+}
+
+function IzBINvDEC(VnosStevila){
+    let stevec = 1;
+    var Stevilo_v_sistem = 0;
+    let dolzina_stevila = VnosStevila.length;
+
+    for (let i = dolzina_stevila - 1; i >= 0; i--) {
+        let branje_stevila = VnosStevila[i];
+        if (branje_stevila === "1") {
+            Stevilo_v_sistem += stevec;
+        }
+        stevec *= 2;
+    }
+    return Stevilo_v_sistem
+}
+
+function BIN(VSistem,VnosStevila) {
+    if(VSistem == "DEC"){
+        var Stevilo_v_sistem = IzBINvDEC(VnosStevila);
+    }else if(VSistem == "OCT"){
+        var Stevilo_v_sistem = IzBINvOCT(VnosStevila);
+    }else if(VSistem == "HEX"){
+        var Stevilo_v_sistem = IzBINvHEX(VnosStevila)
+    }   
    let = document.getElementById("vnos_stevila1").value = Stevilo_v_sistem;
 
+}
+function HEX(VSistem,VnosStevila) {
+    if(VSistem == "DEC"){
+        var Stevilo_v_sistem = IzHEXvBIN(VnosStevila);
+        Stevilo_v_sistem = IzBINvDEC(Stevilo_v_sistem);
+    }else if( VSistem == "BIN"){
+     var Stevilo_v_sistem = IzHEXvBIN(VnosStevila)
+    }else if(VSistem == "OCT"){
+        let bin_stevilo = IzHEXvBIN(VnosStevila)
+        var Stevilo_v_sistem = IzBINvOCT(bin_stevilo);
+        Stevilo_v_sistem = Stevilo_v_sistem.replace(/^0+/, '');
+    }
+let = document.getElementById("vnos_stevila1").value = Stevilo_v_sistem;
+}
+
+function OCT(VSistem,VnosStevila){
+    if( VSistem == "DEC"){
+        var Stevilo_v_sistem = IzOCTvBIN(VnosStevila);
+        Stevilo_v_sistem = IzBINvDEC(Stevilo_v_sistem);
+    }else if(VSistem == "BIN"){
+        var Stevilo_v_sistem = IzOCTvBIN(VnosStevila);
+    }else if(VSistem =="HEX"){
+        var Stevilo_v_sistem = IzOCTvBIN(VnosStevila);
+        var Stevilo_v_sistem = IzBINvHEX(VnosStevila)
+    }
+    let = document.getElementById("vnos_stevila1").value = Stevilo_v_sistem;
+}
+
+function DEC(VSistem,VnosStevila){
+    if(VSistem =="BIN"){
+       var Stevilo_v_sistem = IzDECvBIN(VnosStevila);
+    }else if(VSistem =="OCT"){
+        var Stevilo_v_sistem = IzDECvBIN(VnosStevila);
+        Stevilo_v_sistem = IzBINvOCT(Stevilo_v_sistem);
+    }else if(VSistem =="HEX"){
+        var Stevilo_v_sistem = IzDECvBIN(VnosStevila);
+        Stevilo_v_sistem = IzBINvHEX(Stevilo_v_sistem);
+    }
+    let = document.getElementById("vnos_stevila1").value = Stevilo_v_sistem;
 }
